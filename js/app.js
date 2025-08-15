35f9e4df391777846a194837c088d1033d5c70c9
Ewerthon Savedra <dev.ewerthon@gmail.com>
2025-08-15 15:47:50 -0300

    Criando o arrey de amigos

--- js/app.js
+++ js/app.js
@@ -10,3 +10,18 @@ function adicionar(){
     amigo.value = '';
 }
 
+function sortear(){
+    embaralha(amigo);
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
