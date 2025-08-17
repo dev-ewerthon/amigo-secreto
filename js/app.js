b1cad03f7aace60fecd194583b24224824e4241d
Ewerthon Savedra <dev.ewerthon@gmail.com>
2025-08-17 14:11:49 -0300

    Ativando o botão sortear

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+let amigos = [];
+
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-
+    amigos.push(amigo.value);
+    
     if (lista.textContent == ''){
         lista.textContent = amigo.value;
     } else {
@@ -11,7 +14,17 @@ function adicionar(){
 }
 
 function sortear(){
-    embaralha(amigo);
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++){
+
+        if (i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+    }
 }
 
 function embaralha(lista) {
